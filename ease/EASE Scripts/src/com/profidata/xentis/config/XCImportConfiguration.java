8ca65e03eb86e76e1d1e86d03589423681060561
URR (Reto Urfer) <reto.urfer@profidatagroup.com>
2017-10-27 13:36:16 +0200

    fix XC feature path

--- ease/EASE Scripts/src/com/profidata/xentis/config/XCImportConfiguration.java
+++ ease/EASE Scripts/src/com/profidata/xentis/config/XCImportConfiguration.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class XCImportConfiguration implements ImportConfiguration {
 	private static final ImportConfiguration instance = new XCImportConfiguration();
 
-	private final String rootProjectPath = "xentis";
+	private final String rootProjectPath = "xentisjava";
 	private final Map<String, ImportFeatureProject> features;
 
 	public static ImportConfiguration getInstance() {
@@ -30,13 +30,13 @@ public class XCImportConfiguration implements ImportConfiguration {
 
 	private void configureFeatures() {
 		// XC thirdparty features (content does not have to be imported)
-		features.put("_com.compxc.eclipse.feature", ImportFeatureProject.of("_com.compxc.eclipse.feature", "xentisjava/xc-pde", null));
-		features.put("_com.compxc.eclipse.rcp.feature", ImportFeatureProject.of("_com.compxc.eclipse.rcp.feature", "xentisjava/xc-pde", null));
-		features.put("_com.compxc.eclipse4.spy.feature", ImportFeatureProject.of("_com.compxc.eclipse4.spy.feature", "xentisjava/xc-pde", null));
-		features.put("_com.compxc.eclipse.test.feature", ImportFeatureProject.of("_com.compxc.eclipse.test.feature", "xentisjava/xc-pde", null));
+		features.put("_com.compxc.eclipse.feature", ImportFeatureProject.of("_com.compxc.eclipse.feature", "xc-pde", null));
+		features.put("_com.compxc.eclipse.rcp.feature", ImportFeatureProject.of("_com.compxc.eclipse.rcp.feature", "xc-pde", null));
+		features.put("_com.compxc.eclipse4.spy.feature", ImportFeatureProject.of("_com.compxc.eclipse4.spy.feature", "xc-pde", null));
+		features.put("_com.compxc.eclipse.test.feature", ImportFeatureProject.of("_com.compxc.eclipse.test.feature", "xc-pde", null));
 
 		// XC xentis features
-		features.put("_com.compxc.xentis.feature", ImportFeatureProject.of("_com.compxc.xentis.feature", "xentisjava/xc-pde", null));
-		features.put("_com.compxc.xentis.rcp.feature", ImportFeatureProject.of("_com.compxc.xentis.rcp.feature", "xentisjava/xc-pde", null));
+		features.put("_com.compxc.xentis.feature", ImportFeatureProject.of("_com.compxc.xentis.feature", "xc-pde", null));
+		features.put("_com.compxc.xentis.rcp.feature", ImportFeatureProject.of("_com.compxc.xentis.rcp.feature", "xc-pde", null));
 	}
 }
