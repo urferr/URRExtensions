d493aab02a25dcdf90ccf207730f390dcfb9b740
Reto Urfer <reto.urfer@profidata.com>
2022-10-13 10:18:34 +0200

    add frontend-url definition

--- PDE-Targets & Launcher/oidc/create-realm.sh
+++ PDE-Targets & Launcher/oidc/create-realm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 podman run --name import-xentis --rm \
-	-e KEYCLOAK_URL=http://pdvmdev15.profidatagroup.com:10040/auth \
+	-e KEYCLOAK_URL=http://pdvmdev15:10040/auth \
 	-e KEYCLOAK_USER=admin \
 	-e KEYCLOAK_PASSWORD=admin \
 	-e WAIT_TIME_IN_SECONDS=120 \
@@ -11,5 +11,6 @@ podman run --name import-xentis --rm \
 	-e XENTIS_REALM=$1 \
 	-e XENTIS_BASE_URL=http://pdvmdev15.profidatagroup.com:$2/xentis \
 	-e XENTIS_EXTERNAL_URL=http://pdvmdev15.profidatagroup.com:$3 \
+	-e XENTIS_FRONTEND_URL=http://pdvmdev15.profidatagroup.com:10040/auth \
 	-v $PWD/xentis-realm-template.json:/config/config.json \
 	adorsys/keycloak-config-cli:latest-18.0.0
