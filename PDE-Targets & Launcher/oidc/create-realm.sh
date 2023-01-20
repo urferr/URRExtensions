d09c4be57d404b7f090ed0fc0120d88e3df51041
Reto Urfer <reto.urfer@profidata.com>
2023-01-20 17:20:39 +0100

    adapt to latest state in global repo

--- PDE-Targets & Launcher/oidc/create-realm.sh
+++ PDE-Targets & Launcher/oidc/create-realm.sh
@@ -5,12 +5,12 @@ podman run --name import-xentis --rm \
 	-e KEYCLOAK_USER=admin \
 	-e KEYCLOAK_PASSWORD=admin \
 	-e WAIT_TIME_IN_SECONDS=120 \
-	-e IMPORT_PATH=/config/config.json \
+	-e IMPORT_FILES_LOCATIONS=/config/config.json \
 	-e IMPORT_VARSUBSTITUTION_ENABLED=true \
 	-e IMPORT_FORCE=true \
 	-e XENTIS_REALM=$1 \
 	-e XENTIS_BASE_URL=http://pdvmdev15.profidatagroup.com:$2/xentis \
 	-e XENTIS_EXTERNAL_URL=http://pdvmdev15.profidatagroup.com:$3 \
-	-e XENTIS_FRONTEND_URL=http://pdvmdev15.profidatagroup.com:10040/auth \
+	-e XENTIS_FRONTEND_URL=http://pdvmdev15.profidatagroup.com:$3/auth \
 	-v $PWD/xentis-realm-template.json:/config/config.json \
-	adorsys/keycloak-config-cli:latest-18.0.0
+	adorsys/keycloak-config-cli:5.5.0-20.0.1
