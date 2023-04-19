33da5c95b42bdd6feb80e801e8a38f8729337270
Reto Urfer <reto.urfer@profidata.com>
2023-04-19 15:31:44 +0200

    adapt to current state

--- PDE-Targets & Launcher/oidc/create-realm.sh
+++ PDE-Targets & Launcher/oidc/create-realm.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-podman run --name import-xentis --rm \
-	-e KEYCLOAK_URL=http://pdvmdev15:10040/auth \
-	-e KEYCLOAK_USER=admin \
-	-e KEYCLOAK_PASSWORD=admin \
-	-e WAIT_TIME_IN_SECONDS=120 \
-	-e IMPORT_FILES_LOCATIONS=/config/config.json \
-	-e IMPORT_VARSUBSTITUTION_ENABLED=true \
-	-e IMPORT_FORCE=true \
-	-e XENTIS_REALM=$1 \
-	-e XENTIS_BASE_URL=http://pdvmdev15.profidatagroup.com:$2/xentis \
-	-e XENTIS_EXTERNAL_URL=http://pdvmdev15.profidatagroup.com:$3 \
-	-e XENTIS_FRONTEND_URL=http://pdvmdev15.profidatagroup.com:$3/auth \
-	-v $PWD/xentis-realm-template.json:/config/config.json \
-	adorsys/keycloak-config-cli:5.5.0-20.0.1
+podman exec --env SPRING_CLOUD_CONFIG_LABEL=${1} infra_keycloak_1 bash -ec 'java -Xmx256m -jar /opt/adorsys/keycloak-config-cli.jar \
+    --debug=false \
+	--keycloak.url=http://pdvmdev15:10040/auth \
+	--keycloak.user=admin \
+	--keycloak.password=admin \
+	--import.files.locations=${SPRING_CLOUD_CONFIG_URI:-http://config:8080}/keycloak/${SPRING_PROFILES_ACTIVE:-default}/${SPRING_CLOUD_CONFIG_LABEL:-master}/keycloak/xentis-realm.json'
