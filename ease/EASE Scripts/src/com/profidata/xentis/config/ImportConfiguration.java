cb091176e5be4cd7dbc6c58abd531d069148eda8
Urfer Reto <reto.urfer@profidata.com>
2024-12-19 12:54:29 +0100

    Delete old script implementations wwhich are no longer used

--- ease/EASE Scripts/src/com/profidata/xentis/config/ImportConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.profidata.xentis.config;
-
-public interface ImportConfiguration {
-
-	String getRootProjectPath();
-
-	ImportFeatureProject getFeatureProject(String theProjectName);
-
-}
